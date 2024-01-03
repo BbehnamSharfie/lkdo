81e9430120ff7e0b6bf37b873cf450124d36ea8c
BbehnamSharfie <138737554+BbehnamSharfie@users.noreply.github.com>
2024-01-04 07:46:00 +0800

    Update atna.js

--- atna.js
+++ atna.js
@@ -4,3 +4,12 @@ if (isNaN(age)) {
 } else {
   voteable = (age < 18) ? "Too young" : "Old enough";
 }
+
+const numbers = [45, 4, 9, 16, 25];
+
+let txt = "";
+numbers.forEach(myFunction);
+
+function myFunction(value, index, array) {
+  txt += value;
+}
