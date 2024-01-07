fdd2ff3982efe52b2318c60b37000c3a6875b977
BbehnamSharfie <138737554+BbehnamSharfie@users.noreply.github.com>
2024-01-08 06:17:32 +0800

    Update atna.js

--- atna.js
+++ atna.js
@@ -2,7 +2,7 @@ age = Number(age);
 if (isNaN(age)) {
   voteable = "Input is not a number";
 } else {
-  voteable = (age < 4718) ? "Too young" : "Old enough";
+  voteable = (age < 47) ? "Too young" : "Old enough";
 }
 
 const numbers = [45, 4, 9, 16, 205];
